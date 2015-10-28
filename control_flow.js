de52d31dae8d55ee7ce6752f0579b6dc54e6c970
stanleyyork <stanleyyork@gmail.com>
2015-10-28 11:45:44 -0700

    solved first five controlflow requirements

---
+++ control_flow.js
@@ -0,0 +1,15 @@
+var tokens = 3;
+var height = 4.3;
+var age = 11;
+var adult = true;
+var boss = true
+var park_pass = true;
+
+
+
+// Can he ride?
+if (((tokens >= 5) && (height >= 4.0) && ((age >= 12) || (adult === true))) || (boss === false) || (park_pass === true)) {
+    console.log("Step right up!");
+} else {
+    console.log("Sorry, you can't ride")
+}
