6ba715f0a60f867b815828ce76d006d0f4166b95
stanleyyork <stanleyyork@gmail.com>
2015-10-28 11:48:52 -0700

    print this is awesome 25 times

--- control_flow.js
+++ control_flow.js
@@ -1,15 +1,19 @@
-var tokens = 3;
-var height = 4.3;
-var age = 11;
-var adult = true;
-var boss = true
-var park_pass = true;
+if (false) {
+	var tokens = 3;
+	var height = 4.3;
+	var age = 11;
+	var adult = true;
+	var boss = true
+	var park_pass = true;
 
-
-
-// Can he ride?
-if (((tokens >= 5) && (height >= 4.0) && ((age >= 12) || (adult === true))) || (boss === false) || (park_pass === true)) {
-    console.log("Step right up!");
+	// Can he ride?
+	if (((tokens >= 5) && (height >= 4.0) && ((age >= 12) || (adult === true))) || (boss === false) || (park_pass === true)) {
+	    console.log("Step right up!");
+	} else {
+	    console.log("Sorry, you can't ride");
+	}
 } else {
-    console.log("Sorry, you can't ride")
+	for(var i = 0; i < 25; i++) {
+		console.log(i + ": This is awesome!");
+	}
 }
